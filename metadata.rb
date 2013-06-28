8f78fcfd73e51479a6279694443a701685febc19
John E. Vincent <lusis.org+github.com@gmail.com>
2013-06-28 05:25:36 -0400

    block access to django admin via apache

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lusis.org+github.com@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures gitpaste'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0'
+version          '1.0.1'
 
 recipe "gitpaste", "Installs gitpaste and runs it under defined service"
 recipe "gitpaste::install", "Installs gitpaste"
